69efd0c9f31fbb202dfc4f1613ac909b853b90e8
SaloniVerma09 <salonivermaixd37@gmail.com>
2026-07-31 02:13:36 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+const display = document.getElementById("display");
+
+function appendValue(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0,-1);
+}
+
+function calculate(){
+
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+
+}
+
+document.addEventListener("keydown",function(event){
+
+    const key = event.key;
+
+    if((key >= "0" && key <= "9") ||
+        key=="+" ||
+        key=="-" ||
+        key=="*" ||
+        key=="/" ||
+        key=="." ||
+        key=="%"){
+
+        display.value += key;
+    }
+
+    if(key==="Enter"){
+        event.preventDefault();
+        calculate();
+    }
+
+    if(key==="Backspace"){
+        deleteLast();
+    }
+
+    if(key==="Escape"){
+        clearDisplay();
+    }
+
+});
